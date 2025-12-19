2e55b2ada2278fc061b1d9a57408d4aee49c5b24
sambit <sambitmishra2024+github@gmail.com>
2025-12-19 13:48:41 +0530

    fix: restore calendar.go syntax

--- calendar.go
+++ calendar.go
@@ -7,6 +7,19 @@ import (
 
 	"google.golang.org/api/calendar/v3"
 )
+
+func addContestToCalendar(srv *calendar.Service, contest Contest) {
+	start := time.Unix(contest.StartTimeSeconds, 0)
+	end := start.Add(time.Duration(contest.DurationSeconds) * time.Second)
+
+	event := &calendar.Event{
+		Id:          fmt.Sprintf("cf%d", contest.ID), // Unique ID to prevent duplicates
+		Summary:     contest.Name,
+		Location:    "Codeforces",
+		Description: fmt.Sprintf("https://codeforces.com/contest/%d", contest.ID),
+		Start: &calendar.EventDateTime{
+			DateTime: start.Format(time.RFC3339),
+		},
 		End: &calendar.EventDateTime{
 			DateTime: end.Format(time.RFC3339),
 		},
@@ -20,6 +33,8 @@ import (
 
 	_, err := srv.Events.Insert("primary", event).Do()
 	if err != nil {
+		// If the error is because the event already exists, that's fine (and expected now)
+		// But for now, we just log it.
 		log.Printf("Unable to create event for %s: %v", contest.Name, err)
 		return
 	}
